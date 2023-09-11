dbd56693db3be85532097149032091d2b3295ced
Dan Garfield <dan.garfield@tui.co.uk>
2023-09-11 07:54:51 +0100

    assert reinstate

--- backend/functions/assert-example.js
+++ backend/functions/assert-example.js
@@ -1,5 +1,5 @@
-import sde from '../../frontend/src/generated-data/sde.json' // no assert is ok in netlify
-// import sde from '../../frontend/src/generated-data/sde.json' assert {type:'json'} // assert breaks netlify prod
+// import sde from '../../frontend/src/generated-data/sde.json' // no assert is ok in netlify
+import sde from '../../frontend/src/generated-data/sde.json' assert {type:'json'} // assert breaks netlify prod
 
 export async function handler (event, context) {
   return {

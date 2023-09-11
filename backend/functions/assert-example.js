d3bfff81beaee650db2411b2e20858b61e04183f
Dan Garfield <dan.garfield@tui.co.uk>
2023-09-11 07:49:52 +0100

    assert fix

--- backend/functions/assert-example.js
+++ backend/functions/assert-example.js
@@ -1,5 +1,5 @@
-// import sde from '../../frontend/src/generated-data/sde.json' // assert breaks netlify prod, but is required in heroku
-import sde from '../../frontend/src/generated-data/sde.json' assert {type:'json'} // assert breaks netlify prod, but is required in heroku
+import sde from '../../frontend/src/generated-data/sde.json' // no assert is ok in netlify
+// import sde from '../../frontend/src/generated-data/sde.json' assert {type:'json'} // assert breaks netlify prod
 
 export async function handler (event, context) {
   return {
